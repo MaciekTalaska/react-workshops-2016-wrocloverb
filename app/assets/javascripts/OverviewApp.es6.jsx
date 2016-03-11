48c8979925b74e4b49d9a653c5d0b5153af05818
MaciekTalaska <maciek.talaska@gmail.com>
2016-03-11 13:50:50 +0100

    1 conference shown

---
+++ app/assets/javascripts/OverviewApp.es6.jsx
@@ -0,0 +1,52 @@
+const { $, React, ReactDOM } = window;
+
+function OverviewApp() {
+  const Header = () => {
+    return (<h1>
+              <div className="pull-right">
+                <p><a href="/conferences" className="btn btn-primary">Detailed view</a></p>
+              </div>
+              Conferences
+           </h1> );
+  };
+
+  const ConferenceRow = () => {
+    return (
+      <div className="panel panel-default">
+        <div className="panel-heading">
+          <div className="pull-right">
+            <a href="/conferences/active-conference" className="btn btn-primary btn-xs">Show</a>
+          </div>
+          <h3 className="panel-title">wroc_love.rb 2016</h3>
+        </div>
+      </div>
+    );
+  };
+
+  const Overview = () => {
+    return (<div className="container">
+              <div className="row">
+                <Header/>
+                <ConferenceRow />
+              </div>
+            </div>)
+  };
+
+  return {
+    ui() { 
+      return <Overview/>; 
+    }
+  };
+}
+
+$(() => {
+
+    $("[data-app='Overview']").each(function() {
+
+        const app = OverviewApp();
+
+        ReactDOM.render(app.ui(), this);
+
+    });
+
+});
