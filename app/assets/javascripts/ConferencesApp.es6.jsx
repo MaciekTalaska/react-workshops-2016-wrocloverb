b290d10974c4e696f80274000a728e664ed7fc41
MaciekTalaska <maciek.talaska@gmail.com>
2016-03-11 17:16:27 +0100

    refactoring - better indentation of React components building blocks

--- app/assets/javascripts/ConferencesApp.es6.jsx
+++ app/assets/javascripts/ConferencesApp.es6.jsx
@@ -2,29 +2,35 @@ const { $, React, ReactDOM,
         ReactBootstrap: {Row, Grid, Col, ListGroup,ListGroupItem, Button, ButtonGroup, Panel }} = window;
 
 function ConferencesApp() {
+  const conferenceDetailsData = [
+    {title: "Wroc_love.rb 2016", events: ["Working with Legacy Codebase", "React.js + Redux Workshops"], days: ["Day 1", "Day 2", "Day 3"], active:true},
+    {title: "Wroc_love.rb 2015", events: [], days: [], active:false}
+  ];
+
   const Header = () => {
-    return (<h1>
-              <div className="pull-right">
-                <ButtonGroup>
-                  <Button href="/"
-                          bsStyle="default">Back</Button>
-                  <Button href="/"
-                          bsStyle="primary">Plan new...</Button>
-                </ButtonGroup>        
-              </div>
-              Conferences
-            </h1>
-            );
+    return (
+      <h1>
+        <div className="pull-right">
+          <ButtonGroup>
+            <Button href="/"
+                    bsStyle="default">Back</Button>
+            <Button href="/"
+                    bsStyle="primary">Plan new...</Button>
+          </ButtonGroup>        
+        </div>
+        Conferences
+      </h1>
+    );
   };
 
   const ConferenceDays = () => {
     return (
       <div>
-      <h4>Days:</h4>
-      <ListGroup>
-        <ListGroupItem>Working with Legacy Codebase</ListGroupItem>
-        <ListGroupItem>React.js + Redux Workshops</ListGroupItem>
-      </ListGroup>
+        <h4>Days:</h4>
+        <ListGroup>
+          <ListGroupItem>Working with Legacy Codebase</ListGroupItem>
+          <ListGroupItem>React.js + Redux Workshops</ListGroupItem>
+        </ListGroup>
       </div>
     );
   };
@@ -52,30 +58,31 @@ function ConferencesApp() {
       </div>
     ); 
 
-    return (<Panel header={conferenceHeader}>
-              <Grid>
-                <Row>
-                  <Col md={6}>
-                    <ConferenceDays/>
-                  </Col>
-                  <Col md={6}>
-                    <ConferenceEvents/>
-                  </Col>  
-                </Row>
-              </Grid>
-            </Panel>
+    return (
+      <Panel header={conferenceHeader}>
+        <Grid>
+          <Row>
+            <Col md={6}>
+              <ConferenceDays/>
+            </Col>
+            <Col md={6}>
+              <ConferenceEvents/>
+            </Col>  
+          </Row>
+        </Grid>
+      </Panel>
     );
   };
 
   const Conferences = () => {
-    return (<Grid clasName="container">
-              <Row className="row">
-                <Header/>
-                <ConferenceDetails/>
-              </Row>
-            </Grid>
-            
-            );
+    return (
+      <Grid clasName="container">
+        <Row className="row">
+          <Header/>
+          <ConferenceDetails/>
+        </Row>
+      </Grid>
+    );
   };
 
   return {
